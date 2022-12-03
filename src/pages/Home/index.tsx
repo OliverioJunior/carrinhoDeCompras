78829e550635b34de392ec5cfd76f27e76224e57
Oliverio <oliverio.junior2@gmail.com>
2022-12-02 22:54:12 -0300

    Mudança

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,6 +24,7 @@ interface CartItemsAmount {
 const Home = (): JSX.Element => {
   // const [products, setProducts] = useState<ProductFormatted[]>([]);
   // const { addProduct, cart } = useCart();
+  // const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
   //   // TODO
